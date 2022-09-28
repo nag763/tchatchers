f02710b7f591ec0edd5ffeaef0f70b50e8782a61
nag763 <loic.labeye@tutanota.de>
2022-09-28 19:46:24 +0200

    Clean PG db connection

--- tchatchers_back/tchatchers_core/src/pool.rs
+++ tchatchers_back/tchatchers_core/src/pool.rs
@@ -1,11 +1,16 @@
 use sqlx::postgres::PgPoolOptions;
+use sqlx::postgres::PgConnectOptions;
 use sqlx::PgPool;
 
 pub async fn get_pool() -> PgPool {
-    //    let connection_url = std::env::var("DATABASE_URL").unwrap();
+    let connect_options = PgConnectOptions::new()
+        .host("pg.tchatche.rs")
+        .database(&std::env::var("POSTGRES_DB").expect("No schema defined in .env"))
+        .username(&std::env::var("POSTGRES_USER").expect("No user defined in .env"))
+        .password(&std::env::var("POSTGRES_PASSWORD").expect("No password defined in .env"));
     PgPoolOptions::new()
         .max_connections(15)
-        .connect("postgres://chatter:6@l6QFn5eP56eHA^Ki%IQgaj5BTw7cGd1@postgreshost/chatapp")
+        .connect_with(connect_options)
         .await
         .unwrap()
 }
