afa57a223adca8392d0834c51b818a744dca6ec3
nag763 <loic.labeye@tutanota.de>
2022-10-03 22:13:06 +0200

    Fix toast and submit on mobile

--- tchatchers_front/src/components/toast.rs
+++ tchatchers_front/src/components/toast.rs
@@ -71,7 +71,7 @@ impl Component for Toast {
                     },
         };
         html! {
-                    <div class="absolute bottom-3 right-3 md:bottom-10 md:right-10" hidden={!self.visible}>
+                    <div class="absolute right-3 bottom-5 md:bottom-10 md:right-10" hidden={!self.visible}>
             <div id="toast-success" class="flex items-center p-4 mb-4 w-full max-w-xs text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-zinc-900" role="alert">
                 {icon}
             <div class="ml-3 text-sm font-normal">{self.msg.as_str()}</div>
--- tchatchers_front/src/components/type_bar.rs
+++ tchatchers_front/src/components/type_bar.rs
@@ -95,7 +95,10 @@ impl Component for TypeBar {
             <>
                 <div />
                 <div class="col-span-6 mb-6">
+                <form onsubmit={ctx.link().callback(|_| Msg::SubmitForm)}>
                       <input class="shadow appearance-none border dark:border-zinc-800 rounded-xl px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-zinc-900 w-full h-10 invalid:border-red-500 disabled:bg-gray-100 dark:disabled:bg-zinc-700 focus:invalid:border-red-500 bg-gray-200 dark:bg-zinc-700 dark:text-gray-200" type="text" placeholder={placeholder_input} minlength="2" maxlength="127" ref={self.input_ref.clone()} disabled={!self.can_post} onkeydown={ctx.link().callback(|e : yew::KeyboardEvent | { if e.code() == "Enter" { Msg::SubmitForm } else { Msg::IgnoreEvent }})}/>
+                      <button type="submit" hidden=true></button>
+                  </form>
                       <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700" hidden={self.can_post}>
                         <div class="bg-gradient-to-r from-zinc-600 to-zinc-700 dark:from-zinc-200 dark:to-zinc-300 h-2.5 rounded-full" style={format!("width: {}%", self.progress_percentage*100/TIMEOUT)}>
                         </div>
