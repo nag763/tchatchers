8acf5453aa103c24fb5c255e1aec67c9a4dd2528
nag763 <loic.labeye@tutanota.de>
2022-11-25 23:55:00 +0100

    Refact pfp upload logic

--- tchatchers_back/src/api/pfp.rs
+++ tchatchers_back/src/api/pfp.rs
@@ -15,8 +15,10 @@ use uuid::Uuid;
 ///
 /// - body : the picture content.
 pub async fn upload_pfp(JwtUserExtractor(_): JwtUserExtractor, body: Bytes) -> impl IntoResponse {
-    let file_name = format!("/static/{}.jpg", Uuid::new_v4());
-    let mut file = File::create(format!(".{}", &file_name)).await.unwrap();
+    let file_name = Uuid::new_v4().to_string();
+    let rel_file_path = format!("./static/{}", file_name);
+    let served_file_path: String = format!("/static/{}", file_name);
+    let mut file = File::create(&rel_file_path).await.unwrap();
     file.write_all(&body).await.unwrap();
-    (StatusCode::OK, file_name)
+    (StatusCode::OK, served_file_path)
 }
