d3bfc326e3d080239b5b9da16f706e518aee310b
nag763 <loic.labeye@tutanota.de>
2023-01-22 21:19:19 +0100

    Fix translation issue

--- tchatchers_back/migrations/20230122184404_settings_translations.up.sql
+++ tchatchers_back/migrations/20230122184404_settings_translations.up.sql
@@ -3,4 +3,4 @@ INSERT INTO LABEL(name, default_translation) VALUES
 ('settings', 'Settings');
 
 INSERT INTO TRANSLATION(label_id, locale_id, translation) VALUES
-((SELECT id FROM label WHERE name='settings'), 2, 'Paramètres'),
+((SELECT id FROM label WHERE name='settings'), 2, 'Paramètres');
