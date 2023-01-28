5b1ddb301498c13295843b86363bd7aede089cf7
nag763 <loic.labeye@tutanota.de>
2023-01-28 18:30:15 +0100

    Update core doc

--- tchatchers_core/src/app_context.rs
+++ tchatchers_core/src/app_context.rs
@@ -1,14 +1,30 @@
 // Copyright ⓒ 2022 LABEYE Loïc
 // This tool is distributed under the MIT License, check out [here](https://github.com/nag763/tchatchers/blob/main/LICENSE.MD).
 
+//! The application context is a set of data used by the front-end application to understand who is the current user, and is provided
+//! by the server at the login.
+
 use std::rc::Rc;
 
 use crate::{locale::Locale, navlink::Navlink, translation::Translation, user::PartialUser};
 
+/// The application context.
+///
+/// Contains several important data that the user needs in order to browse the application.
 #[derive(Debug, PartialEq, Eq, Clone, Default, serde::Serialize, serde::Deserialize)]
 pub struct AppContext {
+    /// The user currently being logged in.
     pub user: PartialUser,
+    /// The translations associated to the user's locale.
+    /// 
+    /// Mainly used for internationalization.
     pub translation: Rc<Translation>,
+    /// The list of navigation links he can access to.
+    /// 
+    /// This depends on the user's profile.
     pub navlink: Vec<Navlink>,
+    /// The locales available.
+    /// 
+    /// Mainly used to not request them from the server if the user wants to change his language.
     pub available_locale: Vec<Locale>,
 }
---
+++ tchatchers_front/src/app.rs
@@ -0,0 +1,58 @@
+
+use tchatchers_core::app_context::AppContext;
+use wasm_bindgen::prelude::wasm_bindgen;
+use crate::components::prelude::*;
+
+use crate::router::{switch, Route};
+use crate::utils::requester::Requester;
+use yew::prelude::*;
+use yew::suspense::use_future;
+use yew_router::prelude::*;
+
+#[function_component(ContextualApp)]
+fn contextual_app() -> HtmlResult {
+    let context = use_future(|| async {
+        let req = Requester::<()>::get("/api/app_context");
+        let resp = req.send().await;
+        if resp.status().is_success() {
+            let app_context: AppContext =
+                serde_json::from_str(&resp.text().await.unwrap()).unwrap();
+            Some(app_context)
+        } else {
+            None
+        }
+    })?;
+
+    let context = use_state(|| context.clone());
+
+    Ok(html! {
+        <BrowserRouter>
+            <ContextProvider<UseStateHandle<Option<AppContext>>> context={context}>
+                <div class="h-screen grid grid-rows-12">
+                    <NavbarHOC/>
+                    <Toast />
+                        <Modal />
+                    <div class="row-span-11">
+                        <Switch<Route> render={switch} />
+                    </div>
+                </div>
+            </ContextProvider<UseStateHandle<Option<AppContext>>>>
+        </BrowserRouter>
+    })
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+            <Suspense fallback={html!{ <div>{"Loading ..."}</div>}}>
+                <ContextualApp/>
+            </Suspense>
+        </>
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_app() {
+    yew::Renderer::<App>::new().render();
+}
