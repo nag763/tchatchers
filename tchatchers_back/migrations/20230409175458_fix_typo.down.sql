97594e9aed1f1f5cc6aeba3a34b60cb98ce3a789
nag763 <loic.labeye@tutanota.de>
2023-04-09 21:09:32 +0200

    Fix typo

---
+++ tchatchers_back/migrations/20230409175458_fix_typo.down.sql
@@ -0,0 +1,3 @@
+-- Add down migration script here
+
+UPDATE LABEL SET default_translation = 'You are disconnecetd' WHERE name='you_are_disconnected';
---
+++ tchatchers_back/migrations/20230409175458_fix_typo.up.sql
@@ -0,0 +1,2 @@
+-- Add up migration script here
+UPDATE LABEL SET default_translation = 'You are disconnected' WHERE name='you_are_disconnected';
