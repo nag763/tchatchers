44f8efb4d31713dfaea5694deba429b985323d16
nag763 <loic.labeye@tutanota.de>
2023-06-09 12:37:37 +0200

    Use stable versions

--- tchatchers_front/install_assets.sh
+++ tchatchers_front/install_assets.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-npx tailwindcss -i main.css -o assets/tailwind.css -c tailwind.config.js --minify
+npx tailwindcss@3.3.0 -i main.css -o assets/tailwind.css -c tailwind.config.js --minify
