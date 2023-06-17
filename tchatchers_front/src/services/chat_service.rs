51863a80e097b14a3fcaeb80ad4ce538300f69d7
nag763 <loic.labeye@tutanota.de>
2023-06-17 17:58:59 +0200

    Fix localhost

--- tchatchers_front/src/services/chat_service.rs
+++ tchatchers_front/src/services/chat_service.rs
@@ -17,7 +17,7 @@ pub struct WebsocketService {
 impl WebsocketService {
     pub fn new(room: &str, bearer: &str) -> Self {
         let location = web_sys::window().unwrap().location();
-        let host = "localhost:8080";
+        let host = location.host().unwrap();
         let protocol = location.protocol().unwrap();
         let ws_protocol = match protocol.as_str() {
             "https:" => "wss:",
