d69c61ede93e5984ebb1a22f578a32f0725cccf1
Loïc <55486724+nag763@users.noreply.github.com>
2024-05-25 14:40:50 +0200

    Updates

---
+++ tchatchers_front_refact/src/main.rs
@@ -0,0 +1,18 @@
+use leptos::*;
+
+fn main() {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                // on stable, this is set_count.set(3);
+                set_count(3);
+            }
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {move || count()}
+        </button>
+    }
+}
